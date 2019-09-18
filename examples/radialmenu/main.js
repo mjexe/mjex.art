ef0c1a847c30eb202a0cd7f7faaa1c27fbb24822
Mazz1224 <mazz1224@gmail.com>
2019-09-18 02:25:52 -0400

    'e'

--- examples/radialmenu/main.js
+++ examples/radialmenu/main.js
@@ -1,1 +1,14 @@
-panel = new RadialMenu($('.container'));
+let menu = new RadialMenu($('.container'));
+
+animate();
+
+
+
+
+function animate() {
+	// menu.panel[0].point[1].x = mouseX + 'px ';
+	// menu.panel[0].point[1].y = mouseY + 'px ';
+	// menu.panel[0].apply();
+
+	requestAnimationFrame(() => animate());
+}
--- examples/radialmenu/panel.js
+++ examples/radialmenu/panel.js
@@ -1,14 +1,46 @@
 class RadialMenu {
 	constructor(container) {
 		this.container = $(container)[0];
+		this.panel = [];
 
 		for(let i = 0; i < this.container.children.length; i++) {
-			let color = chroma(rand(0, 360, 0), 0.23, 0.37, 'hsl');
-			this.container.children[i].style.background = color.hex();
-
-			this.container.children[i].style.clipPath = 'polygon(50% 50%, ' + rand(0, 100) + '% ' + rand(0, 100) + '%, ' + rand(0, 100) + '% ' + rand(0, 100) + '%)'
+			this.panel[i] = new Panel(this.container.children[i], {
+				points: [
+					{x: '50%', y: '50%'},
+					{x: rand(0, 100) + '% ', y: rand(0, 100) + '% '},
+					{x: rand(0, 100) + '% ', y: rand(0, 100) + '% '},
+				]
+			});
 		}
 
 		console.log(this.container.children);
+	}
+}
+
+
+
+class Panel {
+	constructor(element, data) {
+		this.element = element;
+
+		this.background = typeof data.background != 'undefined' ? data.background : chroma(rand(0, 360, 0), rand(0.2, 0.5, 2), 0.37, 'hsl').hex();
+
+		this.point = typeof data.points != 'undefined' ? data.points : [
+			{x: 0, y: 0},
+			{x: 0, y: 0},
+			{x: 0, y: 0},
+		];
+
+		this.apply();
+	}
+
+	apply() {
+		this.element.style.background = this.background;
+
+		let path = 'polygon(';
+		this.point.forEach((e, i) => path += e.x + ' ' + e.y + (i < this.point.length - 1 ? ', ' : ''));
+		path += ')'
+
+		this.element.style.clipPath = path;
 	}
 }
