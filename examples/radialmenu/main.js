18bd6344edea9214aba1cd4bb77fb0f047fd73c2
Mazz1224 <mazz1224@gmail.com>
2019-09-18 20:33:43 -0400

    'e'

--- examples/radialmenu/main.js
+++ examples/radialmenu/main.js
@@ -10,13 +10,13 @@ function animate() {
 	let angle1 = angle - (Math.PI / 8);
 	let angle2 = angle + (Math.PI / 8);
 
-	console.log(angle, angle1, angle2);
+	// console.log(angle, angle1, angle2);
 
-	menu.panel[2].point[1].x = (((center.x + (100 * Math.cos(angle1))) / width) * 100) + '%';
-	menu.panel[2].point[1].y = (((center.y + (100 * Math.sin(angle1))) / height) * 100) + '%';
+	menu.panel[2].point[1].x = (((center.x + (calcLength(angle1) * Math.cos(angle1))) / width) * 100) + '%';
+	menu.panel[2].point[1].y = (((center.y + (calcLength(angle1) * Math.sin(angle1))) / height) * 100) + '%';
 
-	menu.panel[2].point[2].x = (((center.x + (100 * Math.cos(angle2))) / width) * 100) + '%';
-	menu.panel[2].point[2].y = (((center.y + (100 * Math.sin(angle2))) / height) * 100) + '%';
+	menu.panel[2].point[2].x = (((center.x + (calcLength(angle2) * Math.cos(angle2))) / width) * 100) + '%';
+	menu.panel[2].point[2].y = (((center.y + (calcLength(angle2) * Math.sin(angle2))) / height) * 100) + '%';
 
 	menu.panel[2].apply();
 
@@ -24,4 +24,4 @@ function animate() {
 }
 
 
-function calcLength(angle) {return center.y / Math.sin(Math.abs(angle))}
+function calcLength(angle) {return center.y / Math.sin(angle)}
