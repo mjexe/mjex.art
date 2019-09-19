02e07681ae46f59c4022077fc187d98eff69ec52
Mazz1224 <mazz1224@gmail.com>
2019-09-18 20:38:06 -0400

    'e'

--- examples/radialmenu/main.js
+++ examples/radialmenu/main.js
@@ -9,14 +9,15 @@ function animate() {
 	let angle = angle2p(center.x, center.y, mouseX, mouseY);
 	let angle1 = angle - (Math.PI / 8);
 	let angle2 = angle + (Math.PI / 8);
+	let length = width > height ? width * 2 : height * 2;
 
 	// console.log(angle, angle1, angle2);
 
-	menu.panel[2].point[1].x = (((center.x + (calcLength(angle1) * Math.cos(angle1))) / width) * 100) + '%';
-	menu.panel[2].point[1].y = (((center.y + (calcLength(angle1) * Math.sin(angle1))) / height) * 100) + '%';
+	menu.panel[2].point[1].x = (((center.x + (length * Math.cos(angle1))) / width) * 100) + '%';
+	menu.panel[2].point[1].y = (((center.y + (length * Math.sin(angle1))) / height) * 100) + '%';
 
-	menu.panel[2].point[2].x = (((center.x + (calcLength(angle2) * Math.cos(angle2))) / width) * 100) + '%';
-	menu.panel[2].point[2].y = (((center.y + (calcLength(angle2) * Math.sin(angle2))) / height) * 100) + '%';
+	menu.panel[2].point[2].x = (((center.x + (length * Math.cos(angle2))) / width) * 100) + '%';
+	menu.panel[2].point[2].y = (((center.y + (length * Math.sin(angle2))) / height) * 100) + '%';
 
 	menu.panel[2].apply();
 
