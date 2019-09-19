373e268f088e8d1ae77f33bdecc3a7ead249d2f5
Mazz1224 <mazz1224@gmail.com>
2019-09-18 21:32:30 -0400

    'e'

--- examples/radialmenu/main.js
+++ examples/radialmenu/main.js
@@ -7,20 +7,18 @@ animate();
 
 function animate() {
 	// let angle = angle2p(center.x, center.y, mouseX, mouseY);
-	let angle = toRadians(30);
-	let angle1 = angle - (Math.PI / 8);
-	let angle2 = angle + (Math.PI / 8);
-	let length = width > height ? width * 2 : height * 2;
+	// let angle = toRadians(30);
+	// let angle1 = angle - (Math.PI / 8);
+	// let angle2 = angle + (Math.PI / 8);
+	// let length = width > height ? width * 2 : height * 2;
 
-	// console.log(angle, angle1, angle2);
+	// menu.panel[2].point[1].x = (((center.x + (length * Math.cos(angle1))) / width) * 100) + '%';
+	// menu.panel[2].point[1].y = (((center.y + (length * Math.sin(angle1))) / height) * 100) + '%';
 
-	menu.panel[2].point[1].x = (((center.x + (length * Math.cos(angle1))) / width) * 100) + '%';
-	menu.panel[2].point[1].y = (((center.y + (length * Math.sin(angle1))) / height) * 100) + '%';
+	// menu.panel[2].point[2].x = (((center.x + (length * Math.cos(angle2))) / width) * 100) + '%';
+	// menu.panel[2].point[2].y = (((center.y + (length * Math.sin(angle2))) / height) * 100) + '%';
 
-	menu.panel[2].point[2].x = (((center.x + (length * Math.cos(angle2))) / width) * 100) + '%';
-	menu.panel[2].point[2].y = (((center.y + (length * Math.sin(angle2))) / height) * 100) + '%';
-
-	menu.panel[2].apply();
+	// menu.panel[2].apply();
 
 	requestAnimationFrame(() => animate());
 }
--- examples/radialmenu/panel.js
+++ examples/radialmenu/panel.js
@@ -5,12 +5,18 @@ class RadialMenu {
 
 		for(let i = 0; i < this.container.children.length; i++) {
 			this.panel[i] = new Panel(this.container.children[i], {
+				background: i == 0 ? '#440000' : '',
 				points: [
 					{x: '50%', y: '50%'},
 					{x: rand(0, 100) + '% ', y: rand(0, 100) + '% '},
 					{x: rand(0, 100) + '% ', y: rand(0, 100) + '% '},
 				]
 			});
+
+			let width = (PI_2 / this.container.children.length);
+			this.panel[i].setAngle(0, PI_2 / 3);
+			this.panel[i].apply();
+			console.log(toDegrees(width * i), toDegrees(width + (width * i)));
 		}
 	}
 }
@@ -28,8 +34,6 @@ class Panel {
 			{x: 0, y: 0},
 			{x: 0, y: 0},
 		];
-
-		this.apply();
 	}
 
 	apply() {
@@ -37,8 +41,22 @@ class Panel {
 
 		let path = 'polygon(';
 		this.point.forEach((e, i) => path += e.x + ' ' + e.y + (i < this.point.length - 1 ? ', ' : ''));
-		path += ')'
+		path += ')';
 
 		this.element.style.clipPath = path;
+	}
+
+	setAngle(angle, width) {
+		let angle1 = angle;
+		let angle2 = angle + width;
+		let length = width > height ? width * 2 : height * 2;
+
+		this.point[1].x = (((center.x + (length * Math.cos(angle1))) / width) * 100) + '%';
+		this.point[1].y = (((center.y + (length * Math.sin(angle1))) / height) * 100) + '%';
+
+		this.point[2].x = (((center.x + (length * Math.cos(angle2))) / width) * 100) + '%';
+		this.point[2].y = (((center.y + (length * Math.sin(angle2))) / height) * 100) + '%';
+
+		this.apply();
 	}
 }
