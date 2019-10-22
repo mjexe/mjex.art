7acb2a1115458e5ad1acc7567101842263088e35
Mazz1224 <mazz1224@gmail.com>
2019-10-22 04:18:15 -0400

    'e'

--- apps/tvs/assets/tv-item.js
+++ apps/tvs/assets/tv-item.js
@@ -1,39 +1,48 @@
 class List {
 	constructor(data) {
 		this.container = $(data.container);
-		this.container.append('<div class="tv-list"></div>');
-		this.list = this.container.find('.tv-list');
+		this.items = [];
 	}
 
-	additem(data) {
-		this.list.append('<div class="item" id="' + data.id + '"></div>');
+	generatelist() {
+		this.container.html('<div class="tv-list"></div>');
+		this.list = this.container.find('.tv-list');
+		this.items.forEach((e, i) => this.renderitem(e));
+		this.list[0].style.gridTemplateColumns = 'repeat(' + hordiv + ', 200px)';
+	}
 
+	generatedetails(id) {
+		let data = this.itemdata(id);
+		this.container.html('<div class="detail-view"></div>');
+		this.detailview = this.container.find('.detail-view');
+	}
+
+	additem(data) {this.items.push(data)}
+
+	additems(array) {
+		array.forEach((e) => this.additem(e));
+	}
+
+	renderitem(data) {
+		this.list.append('<div class="item" id="' + data.id + '"></div>');
 		$('#' + data.id).append(
 			'<div class="top"></div>',
 			'<div class="header">' + data.id + '</div>',
-			'<div class="content"><img src="' + data.img + '"></div>',
+			'<div class="content" style="background: url(\'' + data.img + '"></div>',
+			'<div class=""></div>',
 			'<div class="spacer 1"></div>',
 			'<div class="spacer 2"></div>',
 			'<div class="spacer 3"></div>',
 			'<div class="bottom"></div>',
-		)
-
-		// let item = $('#' + data.id);
-
-		// let contents = [
-		// 	{fn: () => item.append('<div class="top"></div>'),                                 timeout: 0},
-		// 	{fn: () => item.append('<div class="header">' + data.id + '</div>'),               timeout: 200},
-		// 	{fn: () => item.append('<div class="content"><img src="' + data.img + '"></div>'), timeout: 400},
-		// 	{fn: () => item.append('<div class="spacer 1"></div>'),                            timeout: 450},
-		// 	{fn: () => item.append('<div class="spacer 2"></div>'),                            timeout: 500},
-		// 	{fn: () => item.append('<div class="spacer 3"></div>'),                            timeout: 550},
-		// 	{fn: () => item.append('<div class="bottom"></div>'),                              timeout: 600},
-		// ]
-
-		// for(let i = 0; i < contents.length; i++) setTimeout(() => contents[i].fn(), contents[i].timeout);
+		);
 	}
 
-	removeitem(data) {
-		$('#' + data.id).remove();
+	removeitem(id) {
+		this.items.splice(this.getindex(id), 1);
+		// $('#' + id).remove();
 	}
+
+
+	getindex(id) {return this.items.findIndex(x => x.id == id)}
+	itemdata(id) {return this.items[this.getindex(id)]}
 }
--- apps/tvs/main.js
+++ apps/tvs/main.js
@@ -7,33 +7,32 @@ let width,
 
 
 let tvs = new List({container: '.list-container'});
-loadqueue = [];
 
+tvs.additems([
+	{id: 'haha',     img: 'https://i.pinimg.com/236x/09/70/97/09709757a8f52c76abc7c81edaf9446e.jpg'},
+	{id: 'dab',      img: 'https://i.pinimg.com/236x/09/70/97/09709757a8f52c76abc7c81edaf9446e.jpg'},
+	{id: 'dabb',     img: 'https://i.pinimg.com/236x/09/70/97/09709757a8f52c76abc7c81edaf9446e.jpg'},
+	{id: 'dabbb',    img: 'https://i.pinimg.com/236x/09/70/97/09709757a8f52c76abc7c81edaf9446e.jpg'},
+	{id: 'dabbbb',   img: 'https://i.pinimg.com/236x/09/70/97/09709757a8f52c76abc7c81edaf9446e.jpg'},
+	{id: 'dabbbbb',  img: 'https://i.pinimg.com/236x/09/70/97/09709757a8f52c76abc7c81edaf9446e.jpg'},
+]);
 
-loadqueue.push(() => tvs.additem({id: 'haha',     img: 'https://i.pinimg.com/236x/09/70/97/09709757a8f52c76abc7c81edaf9446e.jpg'}));
-loadqueue.push(() => tvs.additem({id: 'dab',      img: 'https://i.pinimg.com/236x/09/70/97/09709757a8f52c76abc7c81edaf9446e.jpg'}));
-loadqueue.push(() => tvs.additem({id: 'dabb',     img: 'https://i.pinimg.com/236x/09/70/97/09709757a8f52c76abc7c81edaf9446e.jpg'}));
-loadqueue.push(() => tvs.additem({id: 'dabbb',    img: 'https://i.pinimg.com/236x/09/70/97/09709757a8f52c76abc7c81edaf9446e.jpg'}));
-loadqueue.push(() => tvs.additem({id: 'dabbbb',   img: 'https://i.pinimg.com/236x/09/70/97/09709757a8f52c76abc7c81edaf9446e.jpg'}));
-loadqueue.push(() => tvs.additem({id: 'dabbbbb',  img: 'https://i.pinimg.com/236x/09/70/97/09709757a8f52c76abc7c81edaf9446e.jpg'}));
 
 
 $(() => {
+	// tvs.generatelist();
 	setStandards();
 	setItemWidth();
-
+	
+	
 	// $('.container > .footer').css('transform', 'translate(0, ' + (301 * verdiv * -1) + ')')
 });
 
 $(window).resize(() => resize());
 
 document.fonts.onloadingdone = () => {
-	// $('.container').animate({opacity: 1}, 250);
-	$('.container')[0].style.opacity = 1
-
-	itemqueue();
-
-	// setTimeout(() => itemqueue(), 250);
+	$('.giga-container')[0].style.opacity = 1;
+	// tvs.generatedetails('haha');
 };
 
 
@@ -51,22 +50,12 @@ function resize() {
 
 function setItemWidth() {
 	hordiv = Math.floor(((width - 224) / 212));
-	verdiv = tvs.list[0].children.length / hordiv;
 	itemwidth = (212 * hordiv);
+	// verdiv = tvs.list[0].children.length / hordiv;
 
-	$('.tv-list')[0].style.gridTemplateColumns = 'repeat(' + hordiv + ', 200px)';
 	$('.container > .header')[0].style.width = itemwidth + 'px';
 	$('.container > .footer')[0].style.width = itemwidth + 'px';
 	$('.giga-container > .titlebar')[0].style.width = (itemwidth + 200) + 'px';
-}
 
-
-
-function itemqueue() {
-	if(loadqueue.length > 0) {
-		let action = loadqueue.splice(0, 1);
-		action[0]();
-	}
-
-	setTimeout(() => itemqueue(), 50);
+	if($('.detail-view').length > 0) $('.detail-view')[0].style.width = (itemwidth - 5) + 'px';
 }
