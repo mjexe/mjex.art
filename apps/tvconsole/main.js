41c473fa64c46f111be28c2d09f8ba8cb71359c7
Mazz1224 <mazz1224@gmail.com>
2019-11-04 14:44:01 -0500

    e

--- apps/tvconsole/main.js
+++ apps/tvconsole/main.js
@@ -1,5 +1,5 @@
 // const socket = io('http://localhost:3000');
-const socket = io('http://mjex.art:3000');
+const socket = io('https://mjex.art:3000');
 
 socket.on('console login accepted', (data) => {
 	console.log('accepted: ', data.session);
