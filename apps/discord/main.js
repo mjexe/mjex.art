eeb92dd484b7b4253fcf450a1a595a81686a7173
BuildTools <unconfigured@null.spigotmc.org>
2020-01-31 00:10:27 -0500

    eee

--- apps/discord/main.js
+++ apps/discord/main.js
@@ -1,11 +1,15 @@
-$.ajax({
-	type: 'post',
+// setInterval(() => {
+	$.ajax({
+		type: 'POST',
+		
+		url: '',
+		
+		data: JSON.stringify({
+			content: '<@186848963373170688>'
+		}),
 	
-	url: 'https://discordapp.com/api/webhooks/672653997672235008/79xRQ508sTP0Lttr6kxiMX6y2wVWGlnhd9PSitTI56ngLJICuWQ9bwdHqH3h5vi0ghIg',
+		success: (data) => {console.log('fuckin worked man')},
 	
-	data: 'my balls hurt',
-
-	success: (data) => {console.log(data)},
-
-	dataType: 'json',
-})
+		contentType: 'application/json',
+	});
+// }, 5000);
