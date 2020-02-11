7f2ccf2d644a0f20b7d6168b238bd2b419471317
BuildTools <unconfigured@null.spigotmc.org>
2020-02-11 05:07:11 -0500

    e

--- examples/irishfest/assets/scripts/main.js
+++ examples/irishfest/assets/scripts/main.js
@@ -4,7 +4,8 @@ let joe;
 $(() => {
 	$('#fullpage').fullpage({
 		licenseKey: 'D4687EE4-ECFE4B2E-88DD07CE-3F05A3D6',
-		anchors: ['firstPage', 'secondPage', '3rdPage'],
+		anchors: ['landing', 'lineup', '3rdPage'],
+		sectionsColor: ['#42ad4e', '#42ad4e', '#34913f', '#2a7833', '#1f5926'],
 
 		navigation: 'true',
 		navigationPosition: 'right',
