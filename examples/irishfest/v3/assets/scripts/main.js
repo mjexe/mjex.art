b4373f707f3987c243da6f2175737211fe13cdcc
BuildTools <unconfigured@null.spigotmc.org>
2020-02-26 12:34:21 -0500

    e

--- examples/irishfest/v3/assets/scripts/main.js
+++ examples/irishfest/v3/assets/scripts/main.js
@@ -5,11 +5,11 @@ $(() => {
 	$('#fullpage').fullpage({
 		licenseKey: 'D4687EE4-ECFE4B2E-88DD07CE-3F05A3D6',
 		anchors: ['landing', 'lineup', '3rdPage'],
-		sectionsColor: ['#42ad4e', '#42ad4e', '#34913f', '#2a7833', '#1f5926'],
+		sectionsColor: ['#42ad4e', '#eeeeee', '#34913f', '#2a7833', '#1f5926'],
 
 		navigation: 'true',
 		navigationPosition: 'right',
-		// scrollingSpeed: 0,
+		scrollingSpeed: 0,
 
 		lazyLoading: false,
 	});
