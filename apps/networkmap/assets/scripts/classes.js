57972c3af8d8e2c629756405cd093048700692cf
Max Lessard <mjexart@gmail.com>
2025-06-22 22:43:47 -0400

    Made a device class that can hold all necessary data

--- apps/networkmap/assets/scripts/classes.js
+++ apps/networkmap/assets/scripts/classes.js
@@ -1,19 +1,36 @@
 class Network {
-    constructor(data) {
-        this.data = data;
-    }
+	constructor(data) {
+		this.data = data;
+	}
 }
 
-class NetSwitch {
-    constructor(data) {
-        this.data = data;
-    }
+class NSwitch {
+	constructor(data, parent) {
+		this.data = data;
+		this.parent = parent;
+	}
 }
 
 class Camera {
-    constructor(data) {
-        this.data = data;
-    }
+	constructor(data) {
+		this.data = data;
+		this.parent = parent;
+	}
 }
 
-export {Network, NetSwitch, Camera}
+class Device {
+	constructor(type, parent, data) {
+		this.type = type;
+		this.parent = parent;
+		this.data = data;
+
+		this.id = self.crypto.randomUUID().substring(0, 8);
+		this.ports = [];
+	}
+
+	addConnection(port, type, data) {
+		this.ports[port] = {'data': data, 'device': new Device(type, this)};
+	}
+}
+
+export {Network, NSwitch, Camera, Device}
--- apps/networkmap/assets/scripts/main.js
+++ apps/networkmap/assets/scripts/main.js
@@ -1,6 +1,20 @@
 //Written from scratch by Max Lessard 6/8/25
-import {Network} from './classes.js';
+import {Network, Device} from './classes.js';
 
-var network = new Network();
+// var network = new Network();
+let network = {};
 
-console.log(network);
+network['10.29.17.0'] = new Device('switch', null);
+network['10.29.17.0'].data = {
+	'name': 'Switch 17 Pit 4 Pillar 2 Switch A',
+	'shorthand': 'PIT4P2A',
+	'zone': 'Pit 4',
+	'location': 'Pit 4 Pillar 2',
+	'switch#': 'Switch A',
+	'subnet': '10.29.17.0',
+}
+
+network['10.29.17.0'].addConnection(1, 'camera');
+
+console.log(network)
+// console.log(sw17);
